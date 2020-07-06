c8539569f7711fc4970f884dd3ad595b023985e5
Grace Su <54904766+graceduansu@users.noreply.github.com>
2020-07-06 19:10:38 -0400

    Added docstring for show_images()

--- disp_multiple_images.py
+++ disp_multiple_images.py
@@ -4,8 +4,16 @@ import numpy as np
 import cv2
 
 
-def show_images(images, titles=None, color=False):
-
+def show_images(images, titles=None, color=False):  
+    """
+    Shows all images and titles in one matplotlib plot.
+    
+    Parameters:
+        images (list): List of CV2 image arrays to be displayed.
+        titles (list): List of titles corresponding to the images list.
+        color (boolean): Displays images in color if True, otherwise displays in grayscale.
+    """
+    
     assert ((titles is None) or (len(images) == len(titles)))
     n_images = len(images)
     cols = math.floor(math.sqrt(n_images))
@@ -25,5 +33,6 @@ def show_images(images, titles=None, color=False):
         else:
             plt.imshow(image)
         a.set_title(title, size=5)
+    
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
     plt.show()
