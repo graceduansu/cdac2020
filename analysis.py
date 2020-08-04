6929828b994c2eeb578d61a1f0c70db23a138bd7
Grace Su <grace.duansu@gmail.com>
2020-08-04 12:39:50 -0400

    added analysis.get_training_curves()

--- analysis.py
+++ analysis.py
@@ -10,11 +10,11 @@ from sklearn.metrics import classification_report, confusion_matrix
 from scipy.stats import entropy
 
 # TODO: save model history and classification report as text files
-def show_classification_report(npz_file, model_path):
+def show_reports(npz_file, encoding_file, model_path):
     """
     RUN WITH -l max
     
-    Displays top 5 accuracy and classification report for a saved Keras model.
+    Generates top 5 accuracy, classification report, confusion matrix, and 16 random incorrectly classified images for a saved Keras model.
     
     Parameters:
         npz_file (str): npz archive of train, valid, test data.
@@ -31,7 +31,7 @@ def show_classification_report(npz_file, model_path):
     print('x_test shape:', x_test.shape)
     
     # Get symbol names
-    symbol_names = np.load("output/label_encoding_073120.npy")
+    symbol_names = np.load(encoding_file)
     print("symbol names:", symbol_names)
     
     y_test_list = []
@@ -123,130 +123,54 @@ def show_classification_report(npz_file, model_path):
     filename = "records/incorrect_imgs_" + date.today().strftime("%m%d%y") + ".png"
     plt.savefig(filename)
 
-def plot_confusion_matrix(npz_file, model_path):
+def get_training_curves(output_file, epochs):
     """
-    RUN WITH -l max
-    
-    Generates and saves confusion matrix for a saved Keras model.
-    
+    Generates training loss and accuracy curves based on model training output text file.
+
     Parameters:
-        npz_file (str): npz archive of train, valid, test data.
-        model_path (str): File path of Keras model to be evaluated.
+        output_file (str): File path of model training output text file.
+        epochs (int): Number of epochs to plot.
     """
 
-    with np.load(npz_file) as data:
-        x_test = data['x_test']
-        y_test = data['y_test']
+    loss = []
+    val_loss = []
+    accuracy = []
+    val_acc = []
 
-    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2],
-                            1)
-    x_test = x_test.astype('float32')
-    print('x_test shape:', x_test.shape)
-    print("y_test:", y_test)
-    
-    # Get symbol names
-    symbol_names = list(DeepScribe.count_symbols())
-    
-    y_test_list = []
-    # Transform y values to corresponding label names
-    for i in range(len(y_test)):
-        s_idx = int(y_test[i])
-        y_test_list.append(symbol_names[s_idx])
-    
-    print("y_test_list:", y_test_list)
+    n = 217
+    with open(output_file, "r", encoding='utf-8') as f:
+        for i in range(n):
+            line = next(f).strip()
 
-    try:
-        model = keras.models.load_model(model_path) 
-    except TypeError:
-        print("Changing model binary file \"learning_rate\" to \"lr\"")
-        f = h5py.File(model_path, "r+")
-        data_p = f.attrs['training_config']
-        data_p = data_p.decode().replace("learning_rate","lr").encode()
-        f.attrs['training_config'] = data_p
-        f.close()
-        print("File modification finished.")
-
-    y_pred = model.predict_classes(x_test)
-    print("y_pred:", y_pred)
-    y_pred_list = []
-    for i in range(len(y_pred)):
-        s_idx = int(y_pred[i])
-        y_pred_list.append(symbol_names[s_idx])
+            l = line.find("loss: ")
+            vl = line.find("val_loss: ")
+            a = line.find("accuracy: ")
+            va = line.find("val_accuracy: ")
+            if l != -1:
+                loss.append(float(line[l+6:a].strip(' -')))
+                accuracy.append(float(line[a+10:vl].strip(' -')))
+                val_loss.append(float(line[vl+10:va].strip(' -')))
+                val_acc.append(float(line[va+14:va+20]))
     
-    print("y_pred_list:", y_pred_list)
+    print(loss)
+    print(accuracy)
+    x = list(range(1, epochs+1))
 
-    confusion = confusion_matrix(x_test, y_pred_list)
-    
-    fig = plt.figure(figsize=(13, 13))
-    ax = fig.add_subplot(111)
-    plt.title("Confusion matrix")
-    cax = ax.matshow(confusion)
-    fig.colorbar(cax)
-
-    ax.set_xticks(range(len(symbol_names)))
-    ax.set_yticks(range(len(symbol_names)))
-    # appending extra tick label to make sure everything aligns properly
-    ax.set_xticklabels(symbol_names)
-    ax.set_yticklabels(symbol_names)
-    filename = "records/confusion_matrix_" + date.today().strftime("%m%d%y") + ".png"
+    plt.plot(x, loss, label="Training loss")
+    plt.plot(x, val_loss, label="Validation loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Model Training Loss")
+    plt.legend(loc="upper right")
+    filename = "records/training_loss_" + date.today().strftime("%m%d%y") + ".png"
     plt.savefig(filename)
+    plt.close()
 
-def plot_incorrect_imgs(npz_file, model_path):
-    """
-    Loads model, runs on test (?) data, picks 16 random incorrectly classified images.
-
-    Parameters:
-        npz_file (str): npz archive of train, valid, test data.
-        model_path (str): File path of Keras model to be evaluated.
-    """
-    with np.load(npz_file) as data:
-        x_test = data['x_test']
-        y_test = data['y_test']
-
-    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2],
-                            1)
-    x_test = x_test.astype('float32')
-
-    # Get symbol names
-    symbol_names = list(DeepScribe.count_symbols())
-
-    try:
-        model = keras.models.load_model(model_path) 
-    except TypeError:
-        print("Changing model binary file \"learning_rate\" to \"lr\"")
-        f = h5py.File(model_path, "r+")
-        data_p = f.attrs['training_config']
-        data_p = data_p.decode().replace("learning_rate","lr").encode()
-        f.attrs['training_config'] = data_p
-        f.close()
-        print("File modification finished.")
-
-    pred_logits = model.predict(x_test)
-    pred_labels = np.argmax(pred_logits, axis=1)
-
-    (incorrect_prediction_idx,) = np.not_equal(
-        y_test, pred_labels
-    ).nonzero()
-
-    incorrect = np.array(incorrect_prediction_idx)
-
-    _, axarr = plt.subplots(5, 5, figsize=(10, 10))
-
-    for i, (ix, iy) in enumerate(np.ndindex(axarr.shape)):
-
-        indx = incorrect[i]
-        img = np.squeeze(x_test[indx, :, :])
-        ground_truth = symbol_names[y_test[indx]]
-        pred_label = symbol_names[pred_labels[indx]]
-        h = entropy(pred_logits[indx, :])
-
-        normalized = h / np.log(len(symbol_names))
-
-        ax = axarr[ix, iy]
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-        ax.set_title(f"P:{pred_label},T:{ground_truth},H:{round(normalized, 2)}")
-        ax.imshow(img, cmap="gray")
-
-    filename = "records/incorrect_imgs_" + date.today().strftime("%m%d%y") + ".png"
+    plt.plot(x, accuracy, label="Training accuracy")
+    plt.plot(x, val_acc, label="Validation accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title("Model Training Accuracy")
+    plt.legend(loc="upper left")
+    filename = "records/training_accuracy_" + date.today().strftime("%m%d%y") + ".png"
     plt.savefig(filename)
--- main.py
+++ main.py
@@ -13,9 +13,8 @@ def run():
     # X = np.load("output/imgs_gray.npy")
     # y = np.load("output/labels_gray.npy")
     # neural_network.train(X, y, "output/resnet18_073120.h5")
-    analysis.show_classification_report("output/split_data_080120_tf230.npz", "output/resnet18_080120_tf230.h5")
-    # analysis.plot_confusion_matrix("output/split_data_073120.npz", "output/resnet18_073120.h5")
-    # analysis.plot_incorrect_imgs("output/split_data_073120.npz", "output/resnet18_073120.h5")
+    # analysis.show_reports("output/split_data_080120_tf230.npz", "output/label_encoding_073120.npy", "output/resnet18_080120_tf230.h5")
+    analysis.get_training_curves("records/training_output_resnet18_tf220.txt", 22)
 
     # Code to build mnist model:
     # X, y = mnist.dict_to_np_arrays("output/img_data1.npy", "output/label_data1.npy") # run with -l max for all symbols
